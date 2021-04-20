6488aa38bf017e2a0ac50e3f53e30236c9101a6f
balaz <balazs_h@hotmail.com>
2021-04-20 19:37:45 +0200

    DigitsCounter

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,8 +1,20 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
 
-    public int getCountOfDigits(Object o) {
-
+    public int getCountOfDigits(String s) {
+        Set<Character> digits = new HashSet<>();
+        if (s == null || s.isBlank()){
+            return 0;
+        }
+        for (char c: s.toCharArray()) {
+            if (Character.isDigit(c)){
+                digits.add(c);
+            }
+        }
+        return digits.size();
     }
 }
