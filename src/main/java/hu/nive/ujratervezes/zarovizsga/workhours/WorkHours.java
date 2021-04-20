74bb515d4f8caf503538f01d4688e18f32a23966
balaz <balazs_h@hotmail.com>
2021-04-20 20:11:21 +0200

    WorkHours

--- src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHours.java
@@ -1,9 +1,35 @@
 package hu.nive.ujratervezes.zarovizsga.workhours;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class WorkHours {
 
 
-    public String minWork(String s) {
+    public String minWork(String s) throws IOException {
+        Path path = Path.of(s);
+        int minLineIndex = 0;
+        int hours = Integer.MAX_VALUE;
+        String result = "";
+        List<String> lines = Files.readAllLines(path);
+        for (int i = 0; i < lines.size(); i++){
+            String[] parts = lines.get(i).split(",");
+            if (Integer.parseInt(parts[1]) < hours){
+                hours = Integer.parseInt(parts[1]);
+                minLineIndex = i;
+            }
+        }
+        String[] minLineParts = lines.get(minLineIndex).split(",");
+        result = minLineParts[0] + ": " + minLineParts[2];
+        return result;
+    }
 
+    public static void main(String[] args) throws IOException {
+        WorkHours workHours = new WorkHours();
+        String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
+        System.out.println(result);
     }
 }
+
--- src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
+++ src/test/java/hu/nive/ujratervezes/zarovizsga/workhours/WorkHoursTest.java
@@ -2,12 +2,14 @@ package hu.nive.ujratervezes.zarovizsga.workhours;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkHoursTest {
 
     @Test
-    void minWork() {
+    void minWork() throws IOException {
         WorkHours workHours = new WorkHours();
         String result = workHours.minWork("src/test/resources/hu/nive/ujratervezes/zarovizsga/workhours/workhours.txt");
         assertEquals("John Doe: 2021-01-04", result);
